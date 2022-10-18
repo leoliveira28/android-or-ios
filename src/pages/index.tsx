a755851336da87f18b88c45f8b56f4ea83dfd3f8
Leoliveira <leandropimentel2011@gmail.com>
2022-10-18 09:07:41 -0300

    teste android

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Head from 'next/head'
 
 import RocketseatLogo from '../assets/rocketseat.svg'
@@ -6,12 +6,13 @@ import RocketseatLogo from '../assets/rocketseat.svg'
 import { Container } from '../styles/pages/Home'
 
 const Home: React.FC = () => {
+  const [device, setDevice] = useState([])
   function androidOrIOS() {
-    console.log(navigator.platform)
+    setDevice([{id: 1, deviceName: navigator.platform}])
     const userAgent = navigator.userAgent;
     if(/android/i.test(userAgent)){
-      console.log('android')
-        return <h1>Android</h1>;
+      setDevice([{id: 1, deviceName: navigator.platform}])
+      return 'android';
     }
     if(/iPad|iPhone|iPod/i.test(userAgent)){
       console.log('ios')
@@ -21,6 +22,9 @@ const Home: React.FC = () => {
 return (
     <div>
       <button onClick={androidOrIOS}>Clique aqui para testar</button>
+      {device.map(device =>
+        <h1>{device.deviceName}</h1>
+        )}
     </div>
   )
 }
