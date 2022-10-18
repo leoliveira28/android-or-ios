35956fd2c57dfbf7728c7f60ed1e395ab77f1088
Leoliveira <leandropimentel2011@gmail.com>
2022-10-18 09:49:04 -0300

    added store links

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,21 @@
 import React, { useState } from 'react'
-import Head from 'next/head'
 
-import RocketseatLogo from '../assets/rocketseat.svg'
 
-import { Container } from '../styles/pages/Home'
+import { useRouter } from 'next/dist/client/router'
 
 const Home: React.FC = () => {
   const [device, setDevice] = useState([])
+  const router = useRouter()
   function androidOrIOS() {
     setDevice([{id: 1, deviceName: navigator.platform}])
     const userAgent = navigator.userAgent;
     if(/android/i.test(userAgent)){
       setDevice([{id: 1, deviceName: 'Sou Android'}])
+      router.push('https://play.google.com/store/apps/details?id=br.com.triibo.segcap')
       return 'android';
     }
     if(/iPad|iPhone|iPod/i.test(userAgent)){
+      router.push('https://apps.apple.com/br/app/sorte-sa%C3%BAde/id1626853627')
       setDevice([{id: 1, deviceName: 'Sou Ios'}])
         return 'ios';
     }
