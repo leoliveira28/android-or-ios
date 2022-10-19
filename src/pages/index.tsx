3f856fcbdc8758a841164815015a84782d085d48
Leoliveira <leandropimentel2011@gmail.com>
2022-10-19 08:43:50 -0300

    added load effecr function

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 
 import { useRouter } from 'next/dist/client/router'
@@ -7,24 +7,29 @@ import { Container } from '../styles/pages/Home'
 const Home: React.FC = () => {
   const [device, setDevice] = useState([])
   const router = useRouter()
-  function androidOrIOS() {
-    setDevice([{id: 1, deviceName: navigator.platform}])
-    const userAgent = navigator.userAgent;
-    if(/android/i.test(userAgent)){
-      setDevice([{id: 1, deviceName: 'Sou Android'}])
-      router.push('https://play.google.com/store/apps/details?id=br.com.triibo.segcap')
-      return 'android';
+
+  useEffect(() => {
+    function androidOrIOS() {
+      const userAgent = navigator.userAgent;
+      if(/android/i.test(userAgent)){
+        setDevice([{id: 1, deviceName: 'Sou Android'}])
+        router.push('https://play.google.com/store/apps/details?id=br.com.triibo.segcap')
+        return 'android';
+      }
+      if(/iPad|iPhone|iPod/i.test(userAgent)){
+        router.push('https://apps.apple.com/br/app/sorte-sa%C3%BAde/id1626853627')
+        setDevice([{id: 1, deviceName: 'Sou Ios'}])
+          return 'ios';
+      }
     }
-    if(/iPad|iPhone|iPod/i.test(userAgent)){
-      router.push('https://apps.apple.com/br/app/sorte-sa%C3%BAde/id1626853627')
-      setDevice([{id: 1, deviceName: 'Sou Ios'}])
-        return 'ios';
-    }
-  }
+  }, [])
+
+
+
 return (
     <div>
       <Container>
-      <button onClick={androidOrIOS}>Clique aqui para testar</button>
+      <h3>Você será direcionado para sua loja de aplicativos</h3>
       </Container>
 
     </div>
